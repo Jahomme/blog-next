526323dd331bffb33ecc0611cc15269920f1551b
Jadilson <jadilsonigor@gmail.com>
2024-01-31 17:46:56 -0300

    Aletarada URL da API

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:1337';
+export const API_URL = 'https://blog-strapi-supabase-app.onrender.com';
 export const POSTS_URL = `${API_URL}/api/posts?populate=*`;
 
 export const SITE_NAME = 'Jadilson Igor';
