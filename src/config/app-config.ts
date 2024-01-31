ce1c3e8d56b0cb9097ff4a7801f2f2ae59e5bb42
Jadilson <jadilsonigor@gmail.com>
2024-01-31 19:10:39 -0300

    adicionando url de produção

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-strapi-supabase-app.onrender.com';
 export const POSTS_URL = `${API_URL}/api/posts?populate=*`;
 
 export const SITE_NAME = 'Jadilson Igor';
-export const SITE_URL = '';
+export const SITE_URL = 'https://darling-kashata-2aceb4.netlify.app';
