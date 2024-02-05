217cbc0a9fe160ad19b2b57fb7939abb26450539
Jadilson <jadilsonigor@gmail.com>
2024-02-05 19:24:52 -0300

    Trocada SITE_URL em app-config

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-strapi-supabase-app.onrender.com';
 export const POSTS_URL = `${API_URL}/api/posts?populate=*`;
 
 export const SITE_NAME = 'Jadilson Igor';
-export const SITE_URL = 'https://darling-kashata-2aceb4.netlify.app';
+export const SITE_URL = 'https://blog-next-2ppi.vercel.app';
